d38a6521f452f73811b9b2eb154af0fe021476f1
Craig-Forrest <Craig.Forrest@ibm.com>
2019-10-20 11:04:16 +0100

    add stage moving and score calc

--- client/src/components/CesiumMap/CesiumMap.jsx
+++ client/src/components/CesiumMap/CesiumMap.jsx
@@ -12,27 +12,49 @@ function radians_to_degrees(radians)
   return radians * (180/pi);
 }
 
-const getScore = async ({ latitude, longitude }, target, feature) => {
-  const latitudeDegrees = radians_to_degrees(latitude)
-    const longitudeDegrees = radians_to_degrees(longitude)
-    const response = await getNearestCity(longitudeDegrees, latitudeDegrees);
-
-    const wavelength = (target[feature] + 1) * 143 / (1 + response[0][feature]);
-    waves(wavelength);
-    console.log("score")
-    const score = wavelength < 143 ? wavelength / 1.43 : 14300 / wavelength
-    console.log(`${score}%`)
+const calcScore = (results) => {
+  if (results.length === 0) return 0;
+  const total = results.reduce((subtotal, result) => subtotal + result.score, 0);
+  return total / results.length;
 }
-class CesiumMap extends React.PureComponent {
 
-  constructor() {
+class CesiumMap extends React.PureComponent {
+  results = [];
+  currentScore = null;
+
+  async getScore({ latitude, longitude }, target, stage) {
+    const latitudeDegrees = radians_to_degrees(latitude)
+      const longitudeDegrees = radians_to_degrees(longitude)
+      const response = await getNearestCity(longitudeDegrees, latitudeDegrees);
+
+      const wavelength = (target[stage] + 1) * 143 / (1 + response[0][stage]);
+      waves(wavelength);
+      const score = wavelength < 143 ? wavelength / 1.43 : 14300 / wavelength
+      console.log("target")
+      console.log(target, target[stage])
+      console.log("score")
+      console.log(`${score}%`)
+      this.currentScore = {name: response[0].name, score, stage};
+  }
+
+  constructor(props) {
     super();
     this.state = {
       started: false,
       stage: 0,
-      stages: ['co2', 'landfill', 'warming']
+      stages: ['co2', 'landfill', 'warming'],
+      city: props.selectedCity,
+      onComplete: props.onComplete,
     }
+  }
 
+  getCameraLocation() {
+    const camera = this.viewer.cesiumElement.scene.camera;
+      const position = camera.positionCartographic;
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      this.timer = setTimeout(() => this.getScore(position, this.state.city, this.state.stages[this.state.stage]), 2000);
   }
 
   componentDidMount() {
@@ -40,18 +62,24 @@ class CesiumMap extends React.PureComponent {
       waves();
       this.setState({ started: true });
     }
-    this.viewer.cesiumElement.scene.camera.changed.addEventListener(() => {
-      const camera = this.viewer.cesiumElement.scene.camera;
-      const position = camera.positionCartographic;
-      if (this.timer) {
-        clearTimeout(this.timer);
-      }
-      this.timer = setTimeout(() => getScore(position, { name: 'London', landfill: 34 }, "landfill"), 2000);
-    });
+    this.viewer.cesiumElement.scene.camera.changed.addEventListener(() => this.getCameraLocation());
   }
 
   render() {
-    return (
+    return [
+        <button onClick={() => {
+          this.state.stage += 1;
+          this.results.push(this.currentScore);
+          this.setState({score: calcScore(this.results)})
+          console.log("this.state.score")
+          console.log(this.state.score)
+          if (this.state.stage >= this.state.stages.length) {
+            this.state.onComplete(this.results);
+          }
+          this.getCameraLocation();
+          console.log(this.results)
+        }}>Inspect camera</button>,
+        <h1>Score: {this.state.score}</h1>,
       <Viewer 
         // baseLayerPicker={false}
         timeline={false}
@@ -63,9 +91,8 @@ class CesiumMap extends React.PureComponent {
         fullscreenButton={false}
         
         ref={e => { this.viewer = e; }} >
-        {/* <button onClick={this.inspectCamera}>Inspect camera</button> */}
       </Viewer>
-    );
+    ];
   }
   inspectCamera = () => {
     const camera = this.viewer.cesiumElement.scene.camera;
