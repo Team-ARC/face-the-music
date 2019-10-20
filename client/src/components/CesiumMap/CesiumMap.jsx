a8ff01cb7d571535f1b2645c186d695ba01d46aa
Craig-Forrest <Craig.Forrest@ibm.com>
2019-10-20 08:23:06 +0100

    add score calculation for map and adjustable waves

--- client/src/components/CesiumMap/CesiumMap.jsx
+++ client/src/components/CesiumMap/CesiumMap.jsx
@@ -2,28 +2,37 @@ import React from 'react';
 import Cesium from "cesium"
 import { Viewer } from "cesium-react";
 import { getNearestCity } from '../../services/location.service';
+import waves from '../Waves';
 
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZTU3ZDZlZi1lNzdiLTQ4MjUtYTliYy1mOTg1MWUyM2JmYTUiLCJpZCI6MTcwMjgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzE0OTc5Mjl9.eAnkhlgA9PGlI4zGdof-ovkLOehYWKIGxdUe4zX9z_U";
+function radians_to_degrees(radians)
+{
+  var pi = Math.PI;
+  return radians * (180/pi);
+}
 
+const getScore = async ({ latitude, longitude }, target, feature) => {
+  const latitudeDegrees = radians_to_degrees(latitude)
+    const longitudeDegrees = radians_to_degrees(longitude)
+    const response = await getNearestCity(longitudeDegrees, latitudeDegrees);
+
+    const wavelength = (target[feature] + 1) * 143 / (1 + response[0][feature]);
+    waves(wavelength);
+    console.log("score")
+    const score = wavelength < 143 ? wavelength / 1.43 : 14300 / wavelength
+    console.log(`${score}%`)
+}
 class CesiumMap extends React.PureComponent {
 
 
   componentDidMount() {
-    function print(data) {
-      console.log(data);
-    }
     this.viewer.cesiumElement.scene.camera.changed.addEventListener(() => {
       const camera = this.viewer.cesiumElement.scene.camera;
       const position = camera.positionCartographic;
       if (this.timer) {
         clearTimeout(this.timer);
       }
-      this.timer = setTimeout(async () => {
-        const { latitude, longitude } = position
-        const response = await getNearestCity(latitude, longitude);
-
-        console.log(response);
-      }, 3000);
+      this.timer = setTimeout(() => getScore(position, { name: 'London', landfill: 34 }, "landfill"), 2000);
     });
   }
 
--- client/src/components/Waves/Waves.jsx
+++ client/src/components/Waves/Waves.jsx
@@ -1,6 +1,6 @@
 import SineWaves from "sine-waves"
 
-const waves = () => {
+const waves = (wavelength) => {
   new SineWaves({
     // Canvas Element
     el: document.getElementById('waves'),
@@ -16,26 +16,24 @@ const waves = () => {
    
     // Specific how much the width of the canvas the waves should be
     // This can either be a number or a percent
-    waveWidth: '95%',
+    waveWidth: '100%',
    
     // An array of wave options
     waves: [
       {
-        timeModifier: 1,   // This is multiplied againse `speed`
-        lineWidth: 3,      // Stroke width
-        amplitude: 150,    // How tall is the wave
-        wavelength: 200,   // How long is the wave
-        segmentLength: 20, // How smooth should the line be
-        strokeStyle: 'rgba(255, 0, 0, 1)', // Stroke color and opacity
-        type: 'sine'       // Wave type
+        timeModifier: 1,
+        lineWidth: 10,
+        amplitude: 150,
+        wavelength: 143,
+        strokeStyle: 'rgba(100, 100, 100, 1)'
       },
       {
-        timeModifier: 1,
-        lineWidth: 2,
-        amplitude: 150,
-        wavelength: 100,
-        strokeStyle: 'rgba(0, 255, 255, 1)'
-      }
+        timeModifier: 1,   // This is multiplied againse `speed`
+        lineWidth: 2,      // Stroke width
+        amplitude: 150,    // How tall is the wave
+        wavelength: wavelength,   // How long is the wave
+        strokeStyle: 'rgba(0, 255, 0, 1)', // Stroke color and opacity
+      },
     ],
    
   })
