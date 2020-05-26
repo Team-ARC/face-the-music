13cf5c9c4c05af9cc02889c3e97e782b436d7331
Richard Waller <Richard.Waller@ibm.com>
2020-05-26 19:23:08 +0100

    refactor: remove obsolete files

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,10 +1,10 @@
 const port = process.env.PORT || 4000;
 
 const origins = {
-    cloud: 'https://face-the-music.eu-gb.cf.appdomain.cloud',
-    netlify: 'https://face-the-music.netlify.app/.netlify/functions/serverless',
-    local: `http://localhost:${port}`,
-  };
+  cloud: 'https://face-the-music.eu-gb.cf.appdomain.cloud',
+  netlify: 'https://face-the-music.netlify.app/.netlify/functions/serverless',
+  local: `http://localhost:${port}`,
+};
 
 const serverLocation = process.env.REACT_APP_SERVER_LOCATION || (process.env.NODE_ENV === 'production' ? 'netlify' : 'local');
 
@@ -13,5 +13,5 @@ const origin = origins[serverLocation];
 console.log(`Client expects server at origin: ${origin} (serverLocation = ${serverLocation})`);
 
 module.exports = {
-    origin,
+  origin,
 }
